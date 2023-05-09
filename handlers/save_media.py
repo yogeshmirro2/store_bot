dedc0cdcde48e9ca17a5c4bfa91b25de81c1c521
yogeshmirro2 <132906109+yogeshmirro2@users.noreply.github.com>
2023-05-09 08:50:27 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -12,7 +12,7 @@ from handlers.database import db
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
 from handlers.multi_channel import get_working_channel_string, get_working_db_channel_id
-
+from handlers.rm import rm_dir,rm_file
 
 async def forward_to_channel(DB_CHANNEL, log_channel, bot: Client, message: Message, editable: Message):
     try:
@@ -83,14 +83,24 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
                 return
         else:
             share_link = share_link1
+        
+        await bot.send_message(
+            chat_id=int(DB_CHANNEL),
+            text=f"#BATCH_SAVE:\n\n[{editable.reply_to_message.from_user.first_name}](tg://user?id={editable.reply_to_message.from_user.id}) Got Batch Link!",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Open Link", url=share_link1)],[InlineKeyboardButton("without shorted Link", url=share_link1)]])
+        )
+        
         if media_thumb_id and photo_send_channel is not None:
-            add_detail = await db.get_add_detail()
             await editable.edit("**sending thumbnail with all Content caption to your VIDEO_PHOTO_SEND channel**")
             #thumb_path = await bot.download_media(media_thumb_id,f"{Config.DOWNLOAD_DIR}/{media_thumb_id}")
+            thumb_path = await bot.download_media(media_thumb_id)
+            add_detail = await db.get_add_detail()
             media_captions1=f"Here is the Permanent Link of your Content: <a href={share_link}>Download Link</a>\n\nJust Click on download to get your Content!\n\nyour Content name are:👇\n\n{media_captions}\n\n{add_detail}" 
-            await bot.send_photo(int(photo_send_channel),media_thumb_id,media_captions1)
+            await bot.send_photo(int(photo_send_channel),thumb_path,media_captions1)
             await editable.edit("**thumbnail with media_captions has been sent to your VIDEO_PHOTO_SEND channel**")
-            await asyncio.sleep(1)
+            await rm_dir()
+            await asyncio.sleep(2)
         await editable.edit(
             f"**Here is the Permanent Link of your Content: <a href={share_link}>Download Link</a>\n\n{media_captions}",
             reply_markup=InlineKeyboardMarkup(
@@ -100,12 +110,7 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
             disable_web_page_preview=True
         )
         
-        await bot.send_message(
-            chat_id=int(DB_CHANNEL),
-            text=f"#BATCH_SAVE:\n\n[{editable.reply_to_message.from_user.first_name}](tg://user?id={editable.reply_to_message.from_user.id}) Got Batch Link!",
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Open Link", url=share_link1)],[InlineKeyboardButton("without shorted Link", url=share_link1)]])
-        )
+        
     except Exception as err:
         await editable.edit(f"Something Went Wrong!\n\n**Error:** `{err}`")
         if log_channel is not None:
@@ -127,7 +132,6 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
         log_channel = await db.check_log_channel_id()
         photo_send_channel = await db.check_video_photo_send()
         Channel_string = await get_working_channel_string()
-        add_detail = await db.get_add_detail()
         each_short_link = await db.check_short_each_link()
     except Exception as err:
         await editable.edit(f"Something Went Wrong!\n\n**Error:** `{err}`")
@@ -145,23 +149,28 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
         else:
             share_link = share_link1
         
-        if forwarded_msg.video and photo_send_channel is not None:
-            await editable.edit("**sending thumbnail with all Content caption to your VIDEO_PHOTO_SEND channel**")
-            media_captions+=f"**👉 {forwarded_msg.caption}**" if forwarded_msg.caption else ""
-            try:
-                thumb_id = forwarded_msg.video.thumbs[0].file_id
-                media_captions1=f"Here is the Permanent Link of your Content: <a href={share_link}>Download Link</a>\n\nJust Click on download to get your Content!\n\nyour Content name are:👇\n\n{media_captions}\n\n{add_detail}"
-                await bot.send_photo(int(photo_send_channel),thumb_id,media_captions1)
-                await editable.edit("**thumbnail with media_captions has been sent to your VIDEO_PHOTO_SEND channel**")
-                await asyncio.sleep(1)
-            except Exception as e:
-                await editable.edit("**can't find thumb id\nfor more error check logs**")
-                print(e)
-                await asyncio.sleep(1)
         await forwarded_msg.reply_text(
             f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!",
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Open Link", url=share_link1)],[InlineKeyboardButton("without shorted Link", url=share_link1)]]),
             disable_web_page_preview=True)
+        
+        
+        if forwarded_msg.video and photo_send_channel is not None:
+            await editable.edit("**sending thumbnail with all Content caption to your VIDEO_PHOTO_SEND channel**")
+            media_captions+=f"**👉 {forwarded_msg.caption}**" if forwarded_msg.caption else ""
+            try:
+                add_detail = await db.get_add_detail()
+                thumb_id = forwarded_msg.video.thumbs[0].file_id
+                thumb_path = await bot.download_media(thumb_id)
+                media_captions1=f"Here is the Permanent Link of your Content: <a href={share_link}>Download Link</a>\n\nJust Click on download to get your Content!\n\nyour Content name are:👇\n\n{media_captions}\n\n{add_detail}"
+                await bot.send_photo(int(photo_send_channel),thumb_path,media_captions1)
+                await editable.edit("**thumbnail with media_captions has been sent to your VIDEO_PHOTO_SEND channel**")
+                await rm_dir()
+                await asyncio.sleep(2)
+            except Exception as e:
+                await editable.edit("**can't find thumb id\nfor more error check logs**")
+                print(e)
+                await asyncio.sleep(1)
         
         
         await editable.edit(
